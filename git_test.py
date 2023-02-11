0a33a46ec387858101f114fa39fdc19d7cd83b7c
Fermata-YOON <98hsyoon@naver.com>
2023-02-11 16:32:25 +0900

    커밋 메세지

---
+++ git_test.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import pandas as pd
+
+print("git_test")
